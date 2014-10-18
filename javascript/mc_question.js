ccb3368f35568cc2fdf641c0b048c6bb17629fe6
Michael Kim <mmkim4@illinois.edu>
2014-10-18 16:09:54 -0400

    committed stuff

--- javascript/mc_question.js
+++ javascript/mc_question.js
@@ -1,5 +1,5 @@
 var quizArray = [
-'26~29~AAAAAAAAA~BBBBBBBBB~CCCCCCCCC~DDDDDDDD~EEEEEEEEEE'
+'64~59~AAAAAAAA~BBBBBBBB~CCCCCCCC~DDDDDDDD~EEEEEEEEE'
 ];
 var qpp = 1; // questions per page to display
 var reslink = ""; // file to link to at end of quiz
