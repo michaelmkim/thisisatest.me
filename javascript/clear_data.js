9ed649e5e1713ef7bb564ba69b8fc73c7e2d4333
Michael Kim <mmkim4@illinois.edu>
2014-10-18 14:04:25 -0400

    sdlfads

---
+++ javascript/clear_data.js
@@ -0,0 +1,11 @@
+var data_source = new Firebase('https://torid-fire-430.firebaseio.com/');
+data_source.remove();
+
+var onComplete = function(error) {
+	if (error)
+		console.log('Synchronization failed');
+	else
+		console.log('Synchronization success');
+};
+
+data_source.remove(onComplete);
