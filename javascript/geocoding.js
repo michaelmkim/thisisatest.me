cfb811114bb9b4aec22a9eb5348642ed441ce3cf
Michael Kim <mmkim4@illinois.edu>
2014-10-18 19:25:57 -0400

    committed stuff

--- javascript/geocoding.js
+++ javascript/geocoding.js
@@ -4,13 +4,17 @@ var city_address = prompt("enter your city");
 var zip_address = prompt("enter your zip code");
 
 
-var geocoder = new google.maps.Geocoder();
-var LatLng;
-var mapOptions = {
+map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
     zoom: 8,
     center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
-map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
 function codeAddress() {
     var address = document.getElementById("address").value;
@@ -18,9 +22,9 @@ function codeAddress() {
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         console.log(results[0].geometry.location); //LatLng
-        LatLng = results[0].geometry.location;
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
     });
 }
+google.maps.event.addDomListener(window, 'load', initialize);
